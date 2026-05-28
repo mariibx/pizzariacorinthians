f020cd4cc2fa99b16dfb5e0e10b0cd987ec0a719
rm99667@estudante.fieb.edu.br <rm99667@estudante.fieb.edu.br>
2026-05-28 07:34:03 -0300

    Criado ambiente de dev

---
+++ src/main/java/com/itb/inf2fm/pizzariacorinthians/PizzariacorinthiansApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzariacorinthians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariacorinthiansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariacorinthiansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2fm/pizzariacorinthians/PizzariacorinthiansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzariacorinthians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariacorinthiansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
