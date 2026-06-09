636bd966b949c664f17a56dc41887136909b0fd1
rm99667@estudante.fieb.edu.br <rm99667@estudante.fieb.edu.br>
2026-06-09 09:51:12 -0300

    package

--- src/main/java/com/itb/inf2fm/pizzariacorinthians/model/Produto.java
+++ src/main/java/com/itb/inf2fm/pizzariacorinthians/model/Produto.java
@@ -10,4 +10,60 @@ public class Produto {
     private BigDecimal valorVenda;
     private int quantidadeEstoque;
     private boolean codStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(BigDecimal valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public BigDecimal getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(BigDecimal valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public boolean isCodStatus() {
+        return codStatus;
+    }
+
+    public void setCodStatus(boolean codStatus) {
+        this.codStatus = codStatus;
+    }
 }
